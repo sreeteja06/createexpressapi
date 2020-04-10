85cb21375b159a75fc2ecc92fa2bfbf06b590ab7
Sree Teja <smutyala@nvestbank.com>
2020-04-10 18:26:21 +0530

    js template change and git inital commit

--- helpers/CreateProject.js
+++ helpers/CreateProject.js
@@ -1,13 +1,46 @@
 const path = require('path');
 const { promisify } = require('util');
 const { ncp } = require('ncp');
+const { exec } = require('child_process');
 
 const CopyDir = promisify(ncp);
 
 const createProject = async (projectName) => {
-  const templatePath = path.join(__dirname, 'template');
+  const templatePath = path.join(__dirname, 'template', 'js');
   const projectPath = path.join(process.cwd(), projectName);
   await CopyDir(templatePath, projectPath);
+  exec(`cd ${projectPath}`, (error, stdout, stderr) => {
+    if (error) {
+      console.log(error);
+      process.exit();
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      process.exit();
+    }
+  });
+  exec(`git add .`, (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      process.exit();
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      process.exit();
+    }
+    console.log(`stdout: ${stdout}`);
+  });
+  exec(`git commit -m "js expressapi boilerplate"`, (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      process.exit();
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      process.exit();
+    }
+    console.log(`stdout: ${stdout}`);
+  });
 };
 
 module.exports = { createProject };
--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 #!/usr/bin/env node
-
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-var */
 /* eslint-disable no-shadow */
+
 const yargs = require('yargs');
 const chalk = require('chalk');
 const { createProject } = require('./helpers/CreateProject');
 
-// eslint-disable-next-line no-var
 var { argv } = yargs
   .usage('usage: createexpressapi <command> [options]')
   .command('create', 'create a new project', (yargs) => {
     argv = yargs
       .usage('usage: createexpressapi create [options]')
-      .command('project', 'create a new project', () => {
-        console.log('creating project :)');
+      .command('project', 'create a new project', (yargs) => {
+        if (argv.n && /^[a-zA-Z0-9 ]+$/i.test(argv.n)) {
+          console.log(chalk.hex('#02d48a').bold('creating project ....'));
+          // console.log(yargs.argv);
+          createProject(argv.n);
+          console.log(chalk.yellowBright.bold('project created :)'));
+        } else {
+          console.log(chalk.red('name is not valid, can contain only alpha numeric with space'));
+        }
       })
       .option('n', {
         alias: 'name',
@@ -26,19 +34,5 @@ var { argv } = yargs
       })
       .wrap(null).argv;
   })
-  // .command('list', 'list items in project', () => {
-  //   console.log('listing items in project :)');
-  // })
   .help('help')
   .wrap(null);
-
-// eslint-disable-next-line no-shadow
-function checkCommands(yargs, argv, numRequired) {
-  if (argv._.length < numRequired) {
-    yargs.showHelp();
-  } else if (argv._.includes('create')) {
-    createProject(argv.n);
-  }
-}
-
-checkCommands(yargs, argv, 1);
